e33cf2154a9856ba5cde651abd30a4a1ff33458d
sumit <sumitjadav2525@gmail.com>
2025-12-30 13:21:36 +0530

    vercel deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve("./src"), 
     },
   },
   build: {
