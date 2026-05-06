0a0eeb37234f6bdb504bf36d368d18de061ba5de
sumit <sumitjadav2525@gmail.com>
2026-05-06 21:21:07 +0530

    frontend React + tailwind  + Routing

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+
+const App = () => {
+  return (
+<>
+<Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          
+        </Routes>
+      </div>
+    </Router>
+</>
+  )
+}
+
+export default App
